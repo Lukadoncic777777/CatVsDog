1d36412d83a12dbc1104d7f3cb8fcff0c42a0375
Lukadoncic777777 <931231099@qq.com>
2025-07-09 16:34:02 +0800

    20250709
    done

--- Main.py
+++ Main.py
@@ -20,35 +20,55 @@ TEST_SIZE=0.6
 EPOCHS=10
 TRANSFORMER=transforms.Compose([transforms.Resize(IMG_WIDTH),transforms.CenterCrop((IMG_HEIGHT,IMG_WIDTH)),transforms.ToTensor()])
 BATCH_SIZE=32
-LR=0.0001
+LR=0.001
+
 device=torch.device("cpu")
 
 
 class Model(nn.Module):
     
     def __init__(self):
-        super(Model,self).__init__()
-        #卷积层
-        self.conv1=nn.Conv2d(3,16,3,padding=1)
-        self.conv2=nn.Conv2d(16,16,3,padding=1)
-        #全连接层
-        self.fc1=nn.Linear(200*200,128)
-        self.fc2=nn.Linear(128,64)
-        self.fc3=nn.Linear(64,2)
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(3, 16, 3, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.conv2 = nn.Conv2d(16, 32, 3, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.conv3 = nn.Conv2d(32, 64, 3, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.Conv2d(64, 128, 3, padding=1)
+        self.bn4 = nn.BatchNorm2d(128)
+        self.conv5 = nn.Conv2d(128, 128, 3, padding=1)
+        self.bn5 = nn.BatchNorm2d(128)
+        self.fc1 = nn.Linear(4608, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.fc3 = nn.Linear(64, 2)
 
-
-
-    def forward(self,x):
-        x=self.conv1(x)
-        x=F.relu(x)
-        x=F.max_pool2d(x,2)
-        x=self.conv2(x)
-        x=F.relu(x)
-        x=F.max_pool2d(x,2)
-        x=x.view(x.size()[0],-1)
-        x=F.relu(self.fc1(x))
-        x=F.relu(self.fc2(x))
-        y=self.fc3(x)
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = self.conv5(x)
+        x = self.bn5(x)
+        x = F.relu(x)
+        x = F.max_pool2d(x, 2)
+        x = x.view(x.size(0), -1)
+        x = F.relu(self.fc1(x))
+        self.dropout = nn.Dropout(0.5)
+        x = F.relu(self.fc2(x))
+        y = self.fc3(x)
         return y
         
 
@@ -80,7 +100,7 @@ def get_model(epochs,images,labels):
 
         print(f'Epoch {epoch + 1}, Loss: {running_loss / len(dataloader)}')
 
-    torch.save(model.state_dict(), 'model.pth')
+    torch.save(model.state_dict(), 'model_new.pth')
     return model
 
 
--- predict.py
+++ predict.py
@@ -1,5 +1,29 @@
+from Main import Model,device,TRANSFORMER
+
+
+from PIL import Image
+import torch
+import sys
+
 def main():
-    return
+
+    model=Model()
+    model.to(device)
+    model.load_state_dict(torch.load(sys.argv[2]))
+    model.eval()
+    # image=TRANSFORMER(Image.open(sys.argv[1]).convert("RGB"))
+    image=Image.open(sys.argv[1]).convert('RGB')
+    input_tensor=TRANSFORMER(image)
+    input_batch = input_tensor.unsqueeze(0).to(device)
+    with torch.no_grad():
+        output = model(input_batch)
+    probabilities = torch.nn.functional.softmax(output,dim=1).squeeze(0)
+    ans,prob='Cat',probabilities[0].item()
+    if probabilities[0].item()<probabilities[1].item():
+        ans,prob='Dog',probabilities[1].item()
+    
+    print(ans,prob)
+    return 
 
 
 if __name__=="__main__":
